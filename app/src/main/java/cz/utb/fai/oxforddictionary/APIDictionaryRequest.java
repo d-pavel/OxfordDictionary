601ae77a4f80b055739576d32d0be1f07a810646
d-pavel <Dupla@seznam.cz>
2019-01-28 13:10:04 +0100

    First screen with connected API - works!

---
+++ app/src/main/java/cz/utb/fai/oxforddictionary/APIDictionaryRequest.java
@@ -0,0 +1,100 @@
+package cz.utb.fai.oxforddictionary;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class APIDictionaryRequest extends AsyncTask<String, Integer, String>
+{
+    // MY API Credentials
+    final String app_id = "0acb2e39";
+    final String app_key = "3926f2b76b9ffcc3cdade01e2795e808";
+
+    String myURl;
+    Context context;
+    TextView vyhledanaData;
+
+    public APIDictionaryRequest(Context context, TextView vyhledanaData)
+    {
+        this.context = context;
+        this.vyhledanaData = vyhledanaData;
+    }
+
+    @Override
+    protected String doInBackground(String... params)
+    {
+        myURl = params[0];
+        try {
+            URL url = new URL(myURl);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept","application/json");
+            urlConnection.setRequestProperty("app_id",app_id);
+            urlConnection.setRequestProperty("app_key",app_key);
+
+            // read the output from the server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+
+            return stringBuilder.toString();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        //Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+
+        String definition;
+        try
+        {
+            JSONObject js = new JSONObject(s);
+            JSONArray results = js.getJSONArray("results");
+
+            JSONObject lexicalEntries = results.getJSONObject(0);
+            JSONArray laArray = lexicalEntries.getJSONArray("lexicalEntries");
+
+            JSONObject entries = laArray.getJSONObject(0);
+            JSONArray e = entries.getJSONArray("entries");
+
+            JSONObject jsonObject = e.getJSONObject(0);
+            JSONArray sensesArray = jsonObject.getJSONArray("senses");
+
+            JSONObject d = sensesArray.getJSONObject(0);
+            JSONArray de = d.getJSONArray("definitions");
+
+            definition = de.getString(0);
+
+            vyhledanaData.setText(definition);
+            //Toast.makeText(context, definition, Toast.LENGTH_SHORT).show();
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/cz/utb/fai/oxforddictionary/MainActivity.java
+++ app/src/main/java/cz/utb/fai/oxforddictionary/MainActivity.java
@@ -2,13 +2,37 @@ package cz.utb.fai.oxforddictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    String url;
+    TextView vyhledanaData;
+    EditText hledaneSlovo;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hledaneSlovo = findViewById(R.id.hledaneSlovo);
+        vyhledanaData = findViewById(R.id.vyhledanaData);
+    }
+
+    public void requestApiButtonClick(View v)
+    {
+        APIDictionaryRequest dictionaryRequest = new APIDictionaryRequest(this, vyhledanaData);
+        url = dictionaryEntries();
+        dictionaryRequest.execute(url);
+    }
+
+    private String dictionaryEntries()
+    {
+        final String language = "en";
+        final String word = hledaneSlovo.getText().toString();
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
     }
 }
